46efa60c24c1bd5c60227a48607c68ed80d22eb9
okshen9 <dev@minimal.localdomain>
2017-11-30 14:11:27 +0300

    skjdfdfbjklg                    ;knljnln                    ;km;km

--- main.cpp
+++ main.cpp
@@ -11,8 +11,15 @@ public:
 
 int main(int argc, char *argv[])
 {
-    cout << "* Create Object base" << endl;
     Base base;
-
+    cout << "* Create Object base" << endl;
+    int s = 0;//summa
+    double sr; //midle value
+    for(int i = 0; i < 10; ++i)
+    {
+        s += i;
+    }//s = 10!
+    sr = s / 10;
+    cout << s << " " << sr << endl;//vivod
     return 0;
 }
